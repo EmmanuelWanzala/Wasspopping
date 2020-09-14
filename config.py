b492662b7bfca7af08c0b5138409df2a319d13a5
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-09-15 00:27:08 +0300

    add API

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
 
-   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey=168f9da938494bd8b2751aa3eace387d'
+   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey=168f9da938494bd8b2751aa3eace387d'
    	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
    	@staticmethod
    	def init_app(app):
